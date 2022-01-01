67dae33bd833e1a9c6a9669e2c194f33376be427
David Pallmann <dpallmann@hotmail.com>
2021-12-31 18:37:11 -0600

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,104 @@
+﻿using Amazon.Athena;
+using Amazon.Athena.Model;
+
+public class Program
+{
+    static AmazonAthenaClient _client = null!;
+
+    const int EmployeeIndex = 0, DateIndex = 1, AmountIndex = 2, ReasonIndex = 3;
+
+    static async Task Main(string[] args)
+    {
+        Console.WriteLine("Issuing query to Athena");
+
+        _client = new AmazonAthenaClient(Amazon.RegionEndpoint.USWest1);
+
+        var queryRequest = new StartQueryExecutionRequest
+        {
+            QueryString = "SELECT employee,date,amount,reason from employee.expense",
+
+            QueryExecutionContext = new QueryExecutionContext()
+            {
+                Database = "employee"
+            },
+            ResultConfiguration = new ResultConfiguration
+            {
+                OutputLocation = "s3://hello-cloud-athena/"
+            }
+        };
+
+        var result = await _client.StartQueryExecutionAsync(queryRequest);
+
+        // Retrieve results
+
+        Console.WriteLine("Retrieving expenses");
+
+        GetQueryResultsResponse results = null!;
+        bool firstTime = true;
+        string token = null!;
+        Row index = null!;
+
+        string queryExecutionId = result.QueryExecutionId;
+        while (firstTime || token != null)
+        {
+            token = (firstTime ? null! : token);
+            results = await GetQueryResult(queryExecutionId, token);
+
+            int skipCount = 0;
+            if (firstTime)
+            {
+                skipCount = 1;
+                index = results.ResultSet.Rows[0];
+            }
+
+            Console.WriteLine($"Employee         Date           Amount Reason");
+            foreach (var exp in results.ResultSet.Rows.Skip(skipCount))
+            {
+                Console.WriteLine($"{exp.Data[EmployeeIndex].VarCharValue,-16} {exp.Data[DateIndex].VarCharValue,-12} {exp.Data[AmountIndex].VarCharValue,8} {exp.Data[ReasonIndex].VarCharValue}"); 
+            }
+
+            token = results.NextToken;
+            firstTime = false;
+        }
+    }
+
+    /// <summary>
+    /// GetQueryResult: Await and return Athena query result. If query is still executing, sleeps and retries up to 10 times.
+    /// </summary>
+    /// <param name="queryExecutionId">Athena query execution Id</param>
+    /// <param name="token">continuation token (should be null the first time)</param>
+    /// <returns>GetQueryResultsResponse object</returns>
+
+    static async Task<GetQueryResultsResponse> GetQueryResult(string queryExecutionId, string token)
+    {
+        GetQueryResultsResponse results = null!;
+        bool succeeded = false;
+        int retries = 0;
+        const int max_retries = 20;
+
+        while (!succeeded)
+        {
+            try
+            {
+                results = await _client.GetQueryResultsAsync(new GetQueryResultsRequest()
+                { QueryExecutionId = queryExecutionId, NextToken = token });
+                succeeded = true;
+            }
+            catch (InvalidRequestException ex)
+            {
+                if (ex.Message.EndsWith("QUEUED") || ex.Message.EndsWith("RUNNING"))
+                {
+                    Thread.Sleep(1000 * 30);
+                    retries++;
+                    if (retries >= max_retries) throw ex;
+                }
+                else
+                {
+                    throw ex;
+                }
+            }
+        }
+
+        return results;
+    }
+}
---
+++ README.md
@@ -0,0 +1,14 @@
+# Hello, Athena!
+
+This is the code project for the [Hello, Athena!](https://davidpallmann.hashnode.dev/hello-athena) blog post. 
+
+This episode: Amazon Athena. In this Hello, Cloud blog series, we're covering the basics of AWS cloud services for newcomers who are .NET developers. If you love C# but are new to AWS, or to this particular service, this should give you a jumpstart.
+
+In this post we'll introduce Amazon Athena and the concept of data lakes, create a "Hello, Cloud" use of Athena, and use it in a .NET program. We'll do this step-by-step, making no assumptions other than familiarity with C# and Visual Studio. We're using Visual Studio 2022 and .NET 6.
+
+## Our Hello, Athena Project
+
+We’re going to create a data lake and upload expense report CSV files to it. Next, we'll teach Athena to understand the data and query the data from the AWS console. Finally, we'll create a .NET console program that queries the data with Athena via the AWS SDK for .NET. 
+
+See the blog post for the tutorial to create this project and run it on AWS.
+
